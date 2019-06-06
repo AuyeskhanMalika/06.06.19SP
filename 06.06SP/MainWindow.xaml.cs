b09a0d1320da3f54bc00cf361983c6ee8d7aa763
auyeskhanm <auyeskhanm@gmail.com>
2019-06-06 21:39:48 +0600

    g

---
+++ 06.06SP/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06._06SP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer timer = new Timer(SaveDoc, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(10));
+
+            Console.ReadLine();
+            timer.Dispose();
+        }
+
+        static void SaveDoc(object state)
+        {
+            
+        }
+
+        private void SaveAsItemClick(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File(*.txt)|*.txt";
+            if(saveFileDialog.ShowDialog()==true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void OpenItemClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void CreateItemClick(object sender, RoutedEventArgs e)
+        {
+            textBox.SelectAll();
+            textBox.Clear();
+        }
+    }
+}
